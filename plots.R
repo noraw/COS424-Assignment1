fa0d0ba52f48c8241aec4b8f8f8de184b46443ed
ruthanium <ruthanium@gmail.com>
2015-02-19 02:59:24 -0500

    basic ROC

--- plots.R
+++ plots.R
@@ -7,6 +7,7 @@
 #
 # *****************************************
 library("ggplot2")
+library("pROC")
 setwd("/Genomics/ogtr04/rd6/COS424/hw1")
 
 # feature variance.
@@ -32,3 +33,23 @@ q2
 ggsave(file="figures/feature_var_before.pdf", plot=q1, width=8, height=5)
 ggsave(file="figures/feature_var_after.pdf", plot=q2, width=8, height=5)
 
+
+# ROC curves
+# ------------------
+y = read.delim("trec07p_data/Test/test_emails_classes_0.txt", header=F)
+predmNB = read.delim("predictions_mNB.txt", header=F)
+predbNB = read.delim("predictions_bNB.txt", header=F)
+
+mat <- data.frame(y, predmNB$V1, predmNB$V2)
+names(mat) <- c("true", "predicted", "score")
+mat <- mat[order(mat$score, decreasing=T),]
+
+mat2 <- data.frame(y, predbNB$V1, predbNB$V2)
+names(mat2) <- c("true", "predicted", "score")
+mat2 <- mat2[order(mat2$score, decreasing=T),]
+
+plot.roc(roc(mat$true, mat$score), col="#1c61b6")
+lines.roc(roc(mat2$true, mat2$score), col="#008600")
+legend("bottomright", legend=c("Multinomial NB", "Bernoulli NB"), col=c("#1c61b6", "#008600"), lwd=2)
+       
+       
