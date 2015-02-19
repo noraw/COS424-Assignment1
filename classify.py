e01d8dfe1d32470086bde599da6a3bbb4c972292
ruthanium <ruthanium@gmail.com>
2015-02-19 02:34:51 -0500

    classify creates output for ROC

--- classify.py
+++ classify.py
@@ -41,30 +41,43 @@ X_test = read_bagofwords_dat("trec07p_data/Test/test_emails_bag_of_words_0.dat",
 y = np.loadtxt(fname=open("trec07p_data/Train/train_emails_classes_200.txt"), dtype=str)
 y_test = np.loadtxt(fname=open("trec07p_data/Test/test_emails_classes_0.txt"), dtype=str)
 
+outname = ""
 # CLASSIFY!
 if args.MultinomialNB:
     print "Multinomial NB"
+    outname = "mNB"
     clf = MultinomialNB();
     MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True) # use defaults
 
 if args.BernoulliNB:
     print "Bernoulli NB"
+    outname = "bNB"
     clf = BernoulliNB()
     BernoulliNB(alpha=1.0, binarize=0.0, class_prior=None, fit_prior=True) # use defaults
 
 if args.SVM:
     print "SVM"
-    clf = SVC()
-    SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
-    gamma=0.0, kernel='rbf', max_iter=-1, probability=False,
-    random_state=None, shrinking=True, tol=0.001, verbose=False) # use defaults
+    outname = "SVM"
+    clf = svm.SVC(kernel='rbf', C=1.0)
+#    SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+#    gamma=0.0, kernel='rbf', max_iter=-1, probability=False,
+#    random_state=None, shrinking=True, tol=0.001, verbose=False) # use defaults
 
 if args.NN:
     print "NN"
+    outname ="NN"
     clf = KNeighborsClassifier(n_neighbors=3);
 
 if args.MultinomialNB or args.BernoulliNB or args.SVM or args.NN:
     clf.fit(X, y)
     predicted = clf.predict(X_test)
+    probs_train = clf.predict_proba(X)
+    probs_test = clf.predict_proba(X_test)
     print "Accuracy: " + str(clf.score(X_test, y_test)) # classifier accuracy
     print(metrics.classification_report(y_test, predicted))
+
+    # output scores for ROC curve
+    outfile = file("predictions_" + outname + ".txt", "w")
+    for i in range (len(predicted)):
+        outfile.write(str(predicted[i]) + "\t" + str(probs_test[i][1]) + "\n") #prob of spam
+    outfile.close();
